6da34b79e33e01dab0db3c349f994ebf5f7f9bad
JeongSH1 <jyt001532@gmail.com>
2026-08-10 18:23:14 +0900

    test: Stream 이벤트 정합성 검증 반영

--- backend/src/test/java/com/dbidding/auction/stream/AuctionBidStreamPersistenceServiceTest.java
+++ backend/src/test/java/com/dbidding/auction/stream/AuctionBidStreamPersistenceServiceTest.java
@@ -47,13 +47,15 @@ class AuctionBidStreamPersistenceServiceTest {
         given(inboxRepository.findByStreamIdIn(anyCollection())).willReturn(List.of());
         given(auctionRepository.findByIdInForUpdate(anyCollection())).willReturn(List.of(auction));
         given(auction.getId()).willReturn(10);
+        given(auction.getLastBidEventVersion()).willReturn(0L);
+        given(auction.isNextBidEventVersion(org.mockito.ArgumentMatchers.anyLong())).willReturn(true);
         given(bidRepository.findByAuctionIdInAndStatus(anyCollection(), org.mockito.ArgumentMatchers.any()))
                 .willReturn(List.of());
         given(auction.applyStreamBid(org.mockito.ArgumentMatchers.anyLong(), org.mockito.ArgumentMatchers.anyLong(),
                 org.mockito.ArgumentMatchers.anyInt(), org.mockito.ArgumentMatchers.any(), org.mockito.ArgumentMatchers.any()))
                 .willReturn(true);
 
-        service.persistAll(List.of(event("1-0", 1L), event("2-0", 2L)));
+        service.persistAll(List.of(event("1-0", 1L, 2, null), event("2-0", 2L, 3, 2)));
 
         verify(inboxRepository, times(1)).findByStreamIdIn(anyCollection());
         verify(auctionRepository, times(1)).findByIdInForUpdate(anyCollection());
@@ -62,9 +64,9 @@ class AuctionBidStreamPersistenceServiceTest {
         verify(bidRepository, times(1)).saveAll(anyList());
     }
 
-    private BidAcceptedStreamEvent event(String streamId, Long version) {
+    private BidAcceptedStreamEvent event(String streamId, Long version, Integer bidderId, Integer previousBidderId) {
         return new BidAcceptedStreamEvent(
-                streamId, BidStreamEventType.BID_ACCEPTED, 10, version, 2, 10_000L + version, null,
+                streamId, BidStreamEventType.BID_ACCEPTED, 10, version, bidderId, 10_000L + version, previousBidderId,
                 "request-" + version, "a".repeat(64), 10_000L + version, version.intValue(),
                 Instant.parse("2027-08-11T12:00:00Z"), com.dbidding.auction.domain.AuctionStatus.OPEN,
                 Instant.parse("2026-08-10T12:00:00Z")
